f31ca53aee0b1f6e82c96c94cbeedb2335b1b837
Nannita1403 <nannimagi@gmail.com>
2024-05-17 23:06:36 +0200

    ok

--- src/api/routes/meme.js
+++
@@ -1,8 +0,0 @@
-const { getMemes, postMeme } = require("../controllers/meme");
-
-const memesRouter= require("express").Router();
-
-memesRouter.get("/", getMemes);
-memesRouter.post("/", postMeme);
-
-module.exports = memesRouter;
